e51f2396f34edfd1b73b1ad5c8897cc202e7e167
bestime <1174295440@qq.com>
2019-08-08 11:26:31 +0800

    sdf

--- src/split/prevent.js
+++ src/split/prevent.js
@@ -1,8 +1,8 @@
 //阻止冒泡及默认行为
 function prevent (ev) {
-    ev = ev ? ev : window.event;
-    window.event? window.event.cancelBubble = true : ev.stopPropagation();
-    window.event? window.event.returnValue = false : ev.preventDefault();
+    ev = ev || window.event;
+    window.event ? window.event.cancelBubble = true : ev.stopPropagation();
+    window.event ? window.event.returnValue = false : ev.preventDefault();
 }
 
 module.exports = prevent
