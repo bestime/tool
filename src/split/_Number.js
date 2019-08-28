73708992d5baa2b0909c887713dd8645327d3375
bestime <1174295440@qq.com>
2019-08-28 09:52:41 +0800

    sdf

--- src/split/_Number.js
+++ src/split/_Number.js
@@ -3,7 +3,7 @@
  * 
  * @param {*} data 需要转换的数据
  * @param {Boolean} canEmpty 是否允许空，处理input输入内容转数字时候可以用到
- * @return {Number || String}
+ * @return {Number|String}
  */
 function _Number (data, canEmpty) {
   var res = Number(data);
@@ -16,32 +16,5 @@ function _Number (data, canEmpty) {
   }
   return res === Infinity || /e/g.test(res) || isNaN(res) ? errorValue : res
 }
-
-/*
-
-
-_Number('+1.25.36')
-_Number('-1.25')
-_Number(0)
-_Number('')
-_Number(false)
-_Number(true)
-_Number({})
-_Number('0')
-_Number([])
-_Number(function () {})
-_Number(/abc/)
-
-_Number(0/1)
-_Number(1/0)
-
-_Number(999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)
-
-_Number('张胜男1啊3二维4')
-_Number('2.张胜男1啊3二维4')
-
-
-
-*/
 
 module.exports = _Number
--- src/split/symbolNumber.js
+++ src/split/symbolNumber.js
@@ -1,39 +1,37 @@
 const _Number = require('./_Number')
+const trim = require('./trim')
 
 /**
  * 
  * @param {*} data 需要转换的数据
  * @param {Boolean} isDecimals 是否小数，如果不是，将自动移除小数点
- * @return {Number || String}
+ * @return {Number|String}
+ * @description 多用于表单数字输入可为 【空字符，正，负】情况下
  */
 function symbolNumber (data, isDecimals) {
-  isDecimals = isDecimals === false ? false : true
   var res = data
-  
-  if(typeof data !== 'number') {
-    var str = data
-    str = String(str).trim()
+  if(typeof data === 'number') {
+    res = _Number(data, true)
+  } else {
+    var str = trim(data)
     var pre = str.substr(0, 1)
     pre = /[0-9+-.]/.test(pre) ? pre : ''; // 保留正负符号
     var hou = str.substring(1)
     // 清空第一个点后后面的点和所有的非数字字符
     if(/\./.test(hou)) {
-      hou = hou.replace(/[^0-9.]+/, '').replace(/\..*/, dot => {
+      hou = hou.replace(/[^0-9.]+/, '').replace(/\..*/, function (dot) {
         return '.' + dot.replace(/[^0-9]/g, '')
       })
     } else {
       hou = hou.replace(/[^0-9]/g, '')
     }
     res = pre + hou
-  } else {
-    res = _Number(data, true)
   }
 
-  if(!isDecimals) {
+  if( isDecimals === false ) {
     res = res.replace(/\./g, '')
   }
-  
-  console.log('结果：', res,'  值：', data)
+
   return res
 }
 
