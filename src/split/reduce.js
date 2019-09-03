0294d0defc8466ea10967bfa3545a84e7be9ea7e
bestime <1174295440@qq.com>
2019-09-03 10:49:54 +0800

    优化reduce方法

--- src/split/reduce.js
+++ src/split/reduce.js
@@ -1,17 +1,24 @@
 const isFunction = require('./isFunction')
+const isArray = require('./isArray')
 
 function reduce (arr, handle, initVal) {
-  if(!isFunction(handle)) return;
-  var res =  '', index = 0;
-  if(typeof initVal !== 'undefined' && initVal !== null) {
-    res = initVal
-    index = 1
+  if(isArray(arr) && isFunction(handle)) {
+    var res = initVal,
+        index = 0,
+        len = arr.length;
+    if(typeof initVal === 'undefined') {
+      res = arr[0]
+      index = 1
+    }
+
+    for (; index < len; index++) {
+      res = handle(res, arr[index])
+    }
+    return res
   }
-  while (index < arr.length) {
-    res = handle(res, arr[index])
-    index++
-  }
-  return res
+
+  // arr 不是数组 或者 handle 不是方法，就返回 initVal
+  return initVal
 }
 
 module.exports = reduce
