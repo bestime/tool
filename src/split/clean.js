c6848664f1771f60db072d1691a2f2c326c7772c
bestime <1174295440@qq.com>
2019-10-25 19:42:37 +0800

    优化clean返回值类型为参数类型

--- src/split/clean.js
+++ src/split/clean.js
@@ -1,13 +1,18 @@
 const getType = require('./getType')
 const filter = require('./filter')
-
+const defaultType = require('./defaultType')
 
 /**
  * 清空[undefined, null]数据
  * @param {*} data
  * @param {Boolean} removeEmptyStr 是否过滤空字符串，默认false
  */
+
 function clean (data, removeEmptyStr) {
+  return defaultType(getType(data), doOnce(data, removeEmptyStr))
+}
+
+function doOnce (data, removeEmptyStr) {
   var res;
   switch (getType(data)) {
     case 'Object':
@@ -15,17 +20,17 @@ function clean (data, removeEmptyStr) {
       var key, item, index = -1;
       for(key in data) {
         item = data[key];
-        typeof item === 'object' && (item = clean(item));
+        typeof item === 'object' && (item = doOnce(item));
         if(canAdd(item)) {
           index++;
           res[key] = item;
         }
       }
-      index === -1 && (res = undefined);
+      index < 0 && (res = undefined);
       break;
     case 'Array':
       res = filter(data, function (item) {
-        typeof item === 'object' && (item = clean(item));
+        typeof item === 'object' && (item = doOnce(item));
         return canAdd(item)
       })
       !res.length && (res = undefined);
@@ -33,8 +38,10 @@ function clean (data, removeEmptyStr) {
     default: res = data;
   }
 
+
+  
   function canAdd (data) {
-    let bol = typeof data !== 'undefined' && data !== null
+    var bol = typeof data !== 'undefined' && data != null
     if (removeEmptyStr && getType(data)==='String') {
       bol = data !== ''
     }
