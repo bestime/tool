be5bf97bc725aa3963bab04563be54707dbd47b9
Bestime <1174295440@qq.com>
2019-11-07 20:14:06 +0800

    sdf

--- src/split/map.js
+++ src/split/map.js
@@ -5,8 +5,10 @@ const clone = require('./clone')
 function map (arr, handle) {
   if(!isFunction(handle)) return;
   var res = [];
-  for(var a=0, len = arr.length; a < len; a++) {
-    res.push(handle(clone(arr[a]), a, arr))
+  if(arr && arr.length) {
+    for(var a=0, len = arr.length; a < len; a++) {
+      res.push(handle(clone(arr[a]), a, arr))
+    }
   }
   return res
 }
--- src/split/some.js
+++ src/split/some.js
@@ -3,12 +3,15 @@ const isFunction = require('./_Function')
 function some (arr, handle) {
   if(!isFunction(handle)) return;
   var res = false;
-  for(var a = 0, len = arr.length; a < len; a++) {
-    if(handle(arr[a], a, arr)) {
-      res = true
-      break;
+  if(arr && arr.length) {
+    for(var a = 0, len = arr.length; a < len; a++) {
+      if(handle(arr[a], a, arr) === true) {
+        res = true
+        break;
+      }
     }
   }
+  
   return res
 }
 
