3b97fc4096a04fdfb079fca321937e0051527995
Bestime <1174295440@qq.com>
2019-11-28 19:23:41 +0800

    sdf

--- src/split/getStorage.js
+++ src/split/getStorage.js
@@ -1,3 +1,9 @@
+/**
+ * 获取本地存储
+ * 
+ * @param {String} key 
+ */
+
 function getStorage (key) {
   var res = localStorage.getItem(key)
   try { res = JSON.parse(res) } catch (e) { }
--- src/split/setStorage.js
+++ src/split/setStorage.js
@@ -1,4 +1,15 @@
+/**
+ * 设置本地存储
+ * 
+ * @param {String} key 
+ * @param {*} val 
+ */
+
 function setStorage (key, val) {
+  // 如果不是字符串，就转为字符串
+  if(typeof val !== 'string') {
+    val = JSON.stringify(val)
+  }
   localStorage.setItem(key, val)
 }
 
