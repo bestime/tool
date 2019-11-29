daa146bd39f7472e777082ac52b83fde65b564c9
Bestime <1174295440@qq.com>
2019-11-29 19:55:48 +0800

    sdf

--- src/split/replaceCustom.js
+++ src/split/replaceCustom.js
@@ -1,16 +1,53 @@
-/**
- * 将字符串中 [start, end] 中间的替换成自定义符号
- * @param {Sting} str 需要替换的字符串
- * @param {Number} start 开始位置
- * @param {Number} end 结束位置
- * @param {String} flag 自定义字符
- */
+var _Number = require('./_Number')
+var _String = require('./_String')
+var isObject = require('./isObject')
 
-function replaceCustom (str, start, end, flag) {
-	var regStr = '(.{0,'+ (start-1) +'})(.{0,'+ (end - start +1 ) +'})(.{1,})';
-	return str.toString().replace(new RegExp(regStr, 'g'), function (a, b, c, d) {
-		return b + c.replace(/./g, flag) + d
-	})
+/*
+replaceCustom({
+	// 处理的字符串个数
+	value: 'abcdefg.exe',
+	
+	// 前面保留的字符串
+	start: 5,
+	
+	// 结束保留的字符串个数
+	end: 0,
+
+	// 无论多少个匹配字符串，都替换这个值
+	mark: '***',
+
+	// 每一个字匹配的符串都替换这个值
+	singleMark: '*', 
+})
+
+*/
+
+function replaceCustom (opt) {
+  var res = '';
+  if(isObject(opt)) {
+    res = _String(opt.value)
+    opt.start = _Number(opt.start)
+    opt.end = _Number(opt.end)
+    opt.mark = opt.mark || '***'
+		var regStr = '^(.{0,'+ opt.start +'})?(.{0,}?)';
+		if(opt.end > 0) {
+			regStr += '(.{0,'+ opt.end +'})'
+		}
+    res = res.replace(new RegExp(regStr + '$'), function (a, $1, $2, $3) {
+			if($2!=='') {
+				if(opt.singleMark) {
+					$2 = $2.replace(/./g, opt.singleMark)
+				} else {
+					$2 = opt.mark
+				}
+			}
+			if(opt.end <= 0) {
+				$3 = ''
+			}
+      return $1 + $2 + $3
+    })
+  }
+  return res;
 }
 
 module.exports = replaceCustom
