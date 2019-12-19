d5e2382637785e56111a4b62fbfcc8321ee8398f
Bestime <1174295440@qq.com>
2019-12-19 16:20:18 +0800

    修复后面相同的cookie键值的获取错误

--- src/split/getCookie.js
+++ src/split/getCookie.js
@@ -2,7 +2,7 @@ const FN_FORMAT_STRING_VALUE = require('./FN_FORMAT_STRING_VALUE')
 //获取cookie
 function getCookie (key) {
   var res;
-  document.cookie.replace(new RegExp(key + '=(.*?)($|(; ))'), function (g, $1) {
+  document.cookie.replace(new RegExp('(^|;\\s)' + key + '=(.*?)($|(; ))'), function (g,prefix, $1) {
     res = FN_FORMAT_STRING_VALUE(decodeURIComponent($1))
   })
   return res
