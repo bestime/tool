aa1e402d8bd0641053e011aa68b7b6883024d58e
Bestime <1174295440@qq.com>
2021-03-04 18:18:27 +0800

    优化indexOf

--- src/split/indexOf.js
+++ src/split/indexOf.js
@@ -1,7 +1,15 @@
-export default function indexOf (arr, data) {
+import isFunction from './isFunction'
+
+/**
+ * 查找指定索引
+ * @param {Array} list 查找的数组
+ * @param {*|Function<Boolean>} compire 对比的数据，或则回调函数
+ * @return {Number} -1表示没找到
+ */
+export default function indexOf (list, compire) {
   var index = 0, findIndex = -1;
-  while (index < arr.length) {
-    if(arr[index]===data) {
+  while (index < list.length) {
+    if(isFunction(compire) ? compire(list[index]) : list[index] === compire) {
       findIndex = index
       break;
     }
