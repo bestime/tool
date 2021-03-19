013648185f3be94f0df7b607a81ec8b732fad648
Bestime <1174295440@qq.com>
2021-03-19 16:49:41 +0800

    日常优化

--- src/split/replaceCustom.js
+++ src/split/replaceCustom.js
@@ -1,52 +1,32 @@
-
-import isObject from './isObject'
 import _String from './_String'
 import _Number from './_Number'
 
-/*
-replaceCustom({
-	// 处理的字符串个数
-	value: 'abcdefg.exe',
-	
-	// 前面保留的字符串
-	start: 5,
-	
-	// 结束保留的字符串个数
-	end: 0,
 
-	// 无论多少个匹配字符串，都替换这个值
-	mark: '***',
-
-	// 每一个字匹配的符串都替换这个值
-	singleMark: '*', 
-})
-
-*/
-
-export default function replaceCustom (opt) {
-  var res = '';
-  if(isObject(opt)) {
-    res = _String(opt.value)
-    opt.start = _Number(opt.start)
-    opt.end = _Number(opt.end)
-    opt.mark = opt.mark || '***'
-		var regStr = '^(.{0,'+ opt.start +'})?(.{0,}?)';
-		if(opt.end > 0) {
-			regStr += '(.{0,'+ opt.end +'})'
+/**
+ * @param {String} value 需要处理的字符串
+ * @param {Number} start 前面保留几位
+ * @param {Number} [end=0] 后面保留几位
+ * @param {String} [mark=null] 中间填充的字符串
+ * @return {String} 处理后的字符串
+ */
+export default function replaceCustom (value, start, end, mark) {
+  var res = _String(value)
+  var regStr = '^(.{0,'+ _Number(start) +'})?(.{0,}?)';
+	end = _Number(end)
+	if(start<=0) {
+		regStr = '^()(.{0,}?)';
+	}
+	if(end > 0) {
+		regStr += '(.{0,'+ end +'})'
+	}
+	res = res.replace(new RegExp(regStr + '$'), function (_, $1, $2, $3) {
+		if($2!=='') {
+			$2 = mark
 		}
-    res = res.replace(new RegExp(regStr + '$'), function (a, $1, $2, $3) {
-			if($2!=='') {
-				if(opt.singleMark) {
-					$2 = $2.replace(/./g, opt.singleMark)
-				} else {
-					$2 = opt.mark
-				}
-			}
-			if(opt.end <= 0) {
-				$3 = ''
-			}
-      return $1 + $2 + $3
-    })
-  }
+		if(end <= 0) {
+			$3 = ''
+		}
+		return $1 + $2 + $3
+	})
   return res;
 }
--- src/split/unique.js
+++ src/split/unique.js
@@ -7,19 +7,20 @@ import { TYPE_NUMBER } from './basic/constant'
  * 一维数组去重，返回新数组
  * @param {Array} arr 需要去重的数组
  * @param {Boolean} [isVague = false] 是否模糊去重; 默认：false; false: [3, '3'] => [3,'3']; true: [3, '3'] => [3];
- * @param {Function} [handle(String key)]循环处理函数，返回比较的key
+ * @param {Function} [handle(String key)=null]循环处理函数，返回比较的key
  */
 
 export default function unique (arr, isVague, handle) {
-  var res = [], exist = {}, checkKey;
+  var res = [], exist = {}, checkKey, itemType;
   forEach(arr, function (item) {
     checkKey = isFunction(handle) ? handle(item) : item
-    checkKey = getType(checkKey) === TYPE_NUMBER || isVague ? checkKey : `k→${checkKey}`
+    itemType = getType(checkKey)
+    checkKey = itemType === TYPE_NUMBER || isVague ? checkKey : `${itemType}→${checkKey}`
     if(!exist[checkKey]) {      
       exist[checkKey] = true;
       res.push(item);
     }
   })
-
+  exist = null
   return res;
 }
