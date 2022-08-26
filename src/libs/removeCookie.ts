4d8fe8303802b56d5cb5c540265cb4291ba1683c
Bestime <1174295440@qq.com>
2022-08-26 18:46:05 +0800

    update

--- src/libs/removeCookie.ts
+++ src/libs/removeCookie.ts
@@ -1,9 +1,8 @@
 
+import setCookie from "./setCookie";
+
 
 //删除cookie
 export default function removeCookie (key: string): void {
-  var oDate = new Date();
-  oDate.setTime(oDate.getTime() - 10000000);
-  var str = key + '=' + encodeURI('') + ';expires=' + oDate.toUTCString();
-  document.cookie = str   
+  setCookie(key, '', -1)  
 }
