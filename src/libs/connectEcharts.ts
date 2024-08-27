886e4e57e66b6a0645f022e58f0827ae218b28d3
IT0909 <1174295440@qq.com>
2024-08-27 10:15:25 +0800

    f

--- src/libs/connectEcharts.ts
+++ src/libs/connectEcharts.ts
@@ -6,48 +6,6 @@ interface IConnectConfigItem {
   onXAxisCtegoryClick?: (xAxisData: any[], tickIndex: number, chart: TEcharts) => void
 }
 
-
-
-function _templateAxxisCategoryClick (iChart: TEcharts, tickIndex: number): Array<any> | undefined {
-  const newOption = cloneEasy(iChart.getOption())
-  if(!newOption) return;
-  if(!isArray(newOption.xAxis)) return void 0;
-  const colorList: string[] = []
-  const splitAreaList = []
-  newOption.xAxis.forEach(function (xAxis: any) {
-    xAxis.data.forEach(function (item: any, idx: number) {
-      item.defaultTextStyle = item.defaultTextStyle ?? item.textStyle
-      if(idx === tickIndex) {
-        colorList.push('rgba(255,255,255,0.2)')
-        item.textStyle = item.activeTextStyle
-        
-      } else {
-        colorList.push('transparent') 
-        item.textStyle = item.defaultTextStyle
-      }
-    })
-    
-    xAxis.splitArea = {
-      show: true,
-      areaStyle: {
-        color: colorList
-      }
-    }
-  })
-  
-  iChart.setOption({
-    xAxis: newOption.xAxis
-  })
-
-  return newOption.xAxis
-  // this.$emit('on:click:xAxis:category', ev.tickIndex, newOption.xAxis)
-  
-  // this.dispatchAction({
-  //   type: 'click.xAxis.category',
-  //   dataIndex: 1
-  // })
-}
-
 interface IListItem {
   config?: IConnectConfigItem
   instence: TEcharts,
@@ -60,11 +18,8 @@ interface IListItem {
 }
 
 class ConnectEcharts {
-
-  _list: Record<string, IListItem[]> = {}
-  
+  _list: Record<string, IListItem[]> = {}  
   constructor () {}
-
 
   _resetGroupClickXAxisCategory (chartList: IListItem[]) {
     chartList.forEach(item => {
@@ -78,8 +33,17 @@ class ConnectEcharts {
         clearTimeout(item.timer_01)
         item.timer_01 = setTimeout(() => {
           chartList.forEach(function (c) {
-            const d = _templateAxxisCategoryClick(c.instence, tickIndex)
-            needEmit && d && c.config && c.config.onXAxisCtegoryClick && c.config.onXAxisCtegoryClick(d.map(c=>c.data), tickIndex, c.instence)
+            const newOption = cloneEasy(c.instence.getOption())
+            if(
+              isArray(newOption.xAxis)
+              && newOption.xAxis.length
+              && needEmit
+              && c.config
+              && c.config.onXAxisCtegoryClick
+            ) {
+              const d = newOption.xAxis as any[]
+              c.config.onXAxisCtegoryClick(d.map(c=>c.data), tickIndex, c.instence)
+            }            
           })
         }, 30)
       }
