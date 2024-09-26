7c1db3e323f1022372066641d38622f544af3375
IT0909 <1174295440@qq.com>
2024-09-26 16:25:54 +0800

    d

--- dist/umd/index.min.d.ts
+++ dist/umd/index.min.d.ts
@@ -289,14 +289,14 @@ declare function floorFixed(
  * 保留几位小数四舍五入
  * toFixed优化版，原生toFixed对于 (3.335).toFixed(2) 结果为：3.33，理想结果为3.34
  * @param data - 长得像数字的字符串或数字
- * @param fractionDigits - 保留的小数位
- * @param rejection - 是否删除末尾所有的 "0"
+ * @param decimals - 保留的小数位
+ * @param removeEndZero - 是否删除末尾所有的 "0"
  * @returns 结果
  */
 declare function roundFixed(
   data: number | string | null | undefined,
-  fractionDigits: number,
-  rejection?: boolean
+  decimals: number,
+  removeEndZero?: boolean
 ): string;
 
 /**
--- src/libs/roundFixed.ts
+++ src/libs/roundFixed.ts
@@ -9,28 +9,36 @@ import { $zeroString } from './help/hpConsts'
  * 保留几位小数四舍五入
  * toFixed优化版，原生toFixed对于 (3.335).toFixed(2) 结果为：3.33，理想结果为3.34
  * @param data - 长得像数字的字符串或数字
- * @param fractionDigits - 保留的小数位
- * @param rejection - 是否删除末尾所有的 "0"
+ * @param decimals - 保留的小数位
+ * @param removeEndZero - 是否删除末尾所有的 "0"
  * @returns 结果
  */
-export default function roundFixed (data: number|string|null|undefined, fractionDigits: number, rejection?: boolean) {
+export default function roundFixed (data: number|string|null|undefined, decimals: number, removeEndZero?: boolean) {
   
   data = _Number(data)
   
   // 保留机位就乘几次10
-  var multiple = Math.pow(10, fractionDigits)
+  var multiple = Math.pow(10, decimals)
+
+  
+
+  // 是否是负数
+  const isFuShu = data < 0
+
+  
 
   //保留机位小数并四舍五入
-  var res = _String(Math.round(data * multiple) / multiple);
+  const prefx = isFuShu ? '-' : '';
+  var res = prefx + _String(Math.round(Math.abs(data * multiple)) / multiple);
 
-  if (fractionDigits > 0) {
+  if (decimals > 0) {
     const chai = split(res, '.')
     const intp = chai[0]; // 整数部分
     var decp = chai[1] || ''; // 小数部分
     
-    if (decp.length < fractionDigits) {
-      decp = padEnd(decp, fractionDigits, $zeroString)
-      if(rejection) {
+    if (decp.length < decimals) {
+      decp = padEnd(decp, decimals, $zeroString)
+      if(removeEndZero) {
         decp = decp.replace(/0+$/, '')
       }
       if(decp) {
