bb99efa423953ba9c3f0b0baeb08cef012a1f51e
IT0909 <1174295440@qq.com>
2025-05-09 17:23:02 +0800

    update

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -62,7 +62,7 @@ export default [
         }
       },
       {
-        file: `dist/esm/index.mjs`,
+        file: `dist/esm/index.min.mjs`,
         banner: getBanner(),
         format: 'esm',
         strict: true,
